5d227a843a8c5e1d825eff07f4bf46a687c14806
amuguelove <908325713@qq.com>
2018-02-07 17:22:10 +0800

    [grpc] commit

--- rpc-benchmark/README.md
+++ rpc-benchmark/README.md
@@ -1,10 +1,24 @@
 GoLang RRC 框架新能对比
 
-go_rpc:
+message: 600 bytes 左右
+
+**go_rpc:**
+
+0ms delay   100 clients  1000000 requests
+
+TPS: 约40000
+total: 20s ~ 30s
+
+
+**grpc:**
+
+0ms delay   100 clients  1000000 requests
+
+TPS: 约12000
+total: 70 ~ 80s
+cpu占用高
 
 
 
-
-jsonrpc:
 
 jsonrpcx:
---
+++ rpc-benchmark/grpc/grpc_server.go
@@ -0,0 +1,49 @@
+/**
+ * @author liangbo
+ * @email  liangbo@codoon.com
+ * @date   2018/2/7 下午4:28
+ */
+package main
+
+import (
+	"time"
+	"flag"
+	"golang.org/x/net/context"
+	"runtime"
+	"net"
+	"log"
+	"google.golang.org/grpc"
+)
+
+type Hello struct{}
+
+func (t *Hello) Say(ctx context.Context, args *BenchmarkMessage) (reply *BenchmarkMessage, err error) {
+	s := "OK"
+	var i int32 = 100
+	args.Field1 = s
+	args.Field2 = i
+	if *delay > 0 {
+		time.Sleep(*delay)
+	} else {
+		runtime.Gosched()
+	}
+	return args, nil
+}
+
+var (
+	host  = flag.String("s", "127.0.0.1:8973", "listened ip and port")
+	delay = flag.Duration("delay", 0, "delay to mock business processing")
+)
+
+func main() {
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", *host)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+
+	RegisterHelloServer(s, &Hello{})
+	s.Serve(lis)
+}
