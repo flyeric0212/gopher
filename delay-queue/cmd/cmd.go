d1f39f61fd8fff579af215f143e8964a269558f1
amuguelove <908325713@qq.com>
2018-02-12 18:03:03 +0800

    commit

---
+++ delay-queue/cmd/cmd.go
@@ -0,0 +1,66 @@
+package cmd
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"blast/push_server/sdk/delay-queue/config"
+	"blast/push_server/sdk/delay-queue/delayqueue"
+	"blast/push_server/sdk/delay-queue/routers"
+)
+
+// Cmd 应用入口Command
+type Cmd struct{}
+
+var (
+	version    bool
+	configFile string
+)
+
+const (
+	// AppVersion 应用版本号
+	AppVersion = "0.4"
+)
+
+// Run 运行应用
+func (cmd *Cmd) Run() {
+
+	// 解析命令行参数
+	cmd.parseCommandArgs()
+	if version {
+		fmt.Println(AppVersion)
+		os.Exit(0)
+	}
+	// 初始化配置
+	config.Init(configFile)
+	// 初始化队列
+	delayqueue.Init()
+
+	// 运行web server
+	cmd.runWeb()
+}
+
+// 解析命令行参数
+func (cmd *Cmd) parseCommandArgs() {
+	// 配置文件
+	flag.StringVar(&configFile, "c", "", "./delay-queue -c /path/to/delay-queue.conf")
+	// 版本
+	flag.BoolVar(&version, "v", false, "./delay-queue -v")
+	flag.Parse()
+}
+
+// 运行Web Server
+func (cmd *Cmd) runWeb() {
+	http.HandleFunc("/push", routers.Push)
+	http.HandleFunc("/pop", routers.Pop)
+	http.HandleFunc("/finish", routers.Delete)
+	http.HandleFunc("/delete", routers.Delete)
+	http.HandleFunc("/get", routers.Get)
+
+	log.Printf("listen %s\n", config.Setting.BindAddress)
+	err := http.ListenAndServe(config.Setting.BindAddress, nil)
+	log.Fatalln(err)
+}
---
+++ delay-queue/main.go
@@ -0,0 +1,13 @@
+/**
+ * @author liangbo
+ * @email  liangbo@codoon.com
+ * @date   2018/2/12 下午5:46
+ */
+package main
+
+import "gopher/delay-queue/cmd"
+
+func main() {
+	cmd := new(cmd.Cmd)
+	cmd.Run()
+}
