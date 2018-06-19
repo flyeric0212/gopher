33b43f506e631eed6ae067a859f5e89128f3c72e
amuguelove <908325713@qq.com>
2018-06-19 11:18:42 +0800

    commit feature2

---
+++ shippy/feature2/consignment-service/main.go
@@ -0,0 +1,86 @@
+/**
+ * @author liangbo
+ * @email  liangbo@codoon.com
+ * @date   2018/6/8 下午5:25
+ */
+package main
+
+import (
+	pb "gopher/shippy/feature2/consignment-service/proto/consignment"
+	"context"
+	"log"
+	"github.com/micro/go-micro"
+)
+
+//
+// 仓库接口
+//
+type IRepository interface {
+	Create(consignment *pb.Consignment) (*pb.Consignment, error) // 存放新货物
+	GetAll() []*pb.Consignment                                   // 获取仓库中所有的货物
+}
+
+//
+// 我们存放多批货物的仓库，实现了 IRepository 接口
+//
+type Repository struct {
+	consignments []*pb.Consignment
+}
+
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	repo.consignments = append(repo.consignments, consignment)
+	return consignment, nil
+}
+
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
+}
+
+//
+// 定义微服务
+//
+type service struct {
+	repo Repository
+}
+
+//
+// 实现 consignment.pb.go 中的 ShippingServiceServer 接口
+//
+
+// 托运新的货物
+// func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, resp *pb.Response) error {
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return err
+	}
+	resp.Created = true
+	resp.Consignment = consignment
+	return nil
+}
+
+// 获取目前所有托运的货物
+// func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, resp *pb.Response) error {
+	allConsignments := s.repo.GetAll()
+	resp = &pb.Response{Consignments: allConsignments}
+	return nil
+}
+
+func main() {
+
+	server := micro.NewService(
+		// 必须和 consignment.proto 中的 package 一致
+		micro.Name("go.micro.srv.consignment"),
+		micro.Version("latest"),
+	)
+
+	// 解析命令行参数
+	server.Init()
+	repo := Repository{}
+	pb.RegisterShippingServiceHandler(server.Server(), &service{repo})
+
+	if err := server.Run(); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
